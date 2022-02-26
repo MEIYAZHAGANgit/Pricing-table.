048a1ddb6f073c092160b6f382d56c21b6ecc5b5
MEIYAZHAGAN <96281339+MEIYAZHAGANgit@users.noreply.github.com>
2022-02-26 13:11:42 +0530

    Add files via upload

---
+++ Greet.js
@@ -0,0 +1,10 @@
+import App from '.App';
+import reportwebVitals from '.reportWebVitals';
+
+ReactDOM.render(
+ <React.StrictMode>
+     <App title ="Welcome React!!" />
+ </React.StrictMode>,
+ document.getElementById('root')
+);
+reportwebVitals();
